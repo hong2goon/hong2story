9ea9e38c6322ff527026c49f7670091b54c8d76b
hong2goon <hong2goon@email.com>
2023-03-15 16:35:39 +0900

    ...

---
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -0,0 +1,13 @@
+import './PageTemplate.scss';
+
+function PageTemplate() {
+  return (
+    <div className="page-wrap">
+      <header>헤더</header>
+      <main>컨텐츠</main>
+      <footer>푸터</footer>
+    </div>
+  )
+}
+
+export default PageTemplate;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import PageTemplate from "../components/common/PageTemplate/PageTemplate";
+
+function Home() {
+  return (
+    <PageTemplate>
+      home page
+    </PageTemplate>
+  )
+}
+
+export default Home;
