d32bdd3be1ff4894085bc9aad7a7b21b27ea2289
hong2goon <hong2goon@email.com>
2023-03-17 14:47:40 +0900

    html title 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React, { useEffect } from 'react';
 import Routers from './components/router/Routers';
 import './App.scss';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Publishing Guides'
+  }, []);
+
   return (
     <div className="App">
       <Routers />
